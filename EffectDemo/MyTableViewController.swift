5c5b58815ffab5d0338a233f6e975e959ad72acc
ittmomWang <15130886370@163.com>
2018-08-08 16:26:46 +0800

    迁移到 Swift 4.0

--- EffectDemo/MyTableViewController.swift
+++ EffectDemo/MyTableViewController.swift
@@ -15,76 +15,22 @@ class MyTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: identifier)
-        navigationController?.navigationBar.translucent = false
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        navigationController?.navigationBar.isTranslucent = false
     }
 
     // MARK: - Table view data source
-
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 30
     }
 
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         cell.textLabel?.text = "\(self.title!)-\(indexPath.row)"
         return cell
     }
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- EffectDemo/TMHomeController.swift
+++ EffectDemo/TMHomeController.swift
@@ -8,20 +8,13 @@
 
 import UIKit
 
-public let SCREENW:CGFloat = UIScreen.mainScreen().bounds.width
-public let SCREENH: CGFloat = UIScreen.mainScreen().bounds.height
-
 class TMHomeController: UIViewController {
 
-    
     @IBOutlet weak var titleScroll: UIScrollView!
     
     @IBOutlet weak var contentScroll: UIScrollView!
-    
-    
-    
-    override func viewDidLoad()
-    {
+
+    override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false//关掉系统自动调节scrollView的位置
         
@@ -34,13 +27,7 @@ class TMHomeController: UIViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    private func setUpChildVC()
-    {
+    private func setUpChildVC() {
         let vc1 = MyTableViewController()
         vc1.title = "国际"
         addChildViewController(vc1)
@@ -50,145 +37,91 @@ class TMHomeController: UIViewController {
         let vc3 = MyTableViewController()
         vc3.title = "社会"
         addChildViewController(vc3)
-        let vc4 = MyTableViewController()
-        vc4.title = "政治"
-        addChildViewController(vc4)
-        let vc5 = MyTableViewController()
-        vc5.title = "经济"
-        addChildViewController(vc5)
-        let vc6 = MyTableViewController()
-        vc6.title = "体育"
-        addChildViewController(vc6)
-        let vc7 = MyTableViewController()
-        vc7.title = "娱乐"
-        addChildViewController(vc7)
     }
 
-    private func setUpTitle()
-    {
-        let labelWidth: CGFloat = 100.0
+    private func setUpTitle() {
+        let labelWidth: CGFloat = UIScreen.main.bounds.width / 3
         
-        for i in 0..<7
-        {
-            let title = UILabel(frame: CGRectMake(labelWidth * CGFloat(i), 0, labelWidth, 34))
+        for i in 0 ..< 3 {
+            let title = UILabel(frame: CGRect(x: labelWidth * CGFloat(i), y: 0, width: labelWidth, height: 34))
             title.text = childViewControllers[i].title
-            title.backgroundColor = UIColor.grayColor()
-            title.textAlignment = .Center
+            title.backgroundColor = UIColor.gray
+            title.textAlignment = .center
             title.tag = 100 + i
             titleScroll.addSubview(title)
-            title.userInteractionEnabled = true
-            let guest = UITapGestureRecognizer.init(target: self, action: .labelTap)
+            title.isUserInteractionEnabled = true
+            let guest = UITapGestureRecognizer.init(target: self, action: #selector(labelTap(tap:)))
             title.addGestureRecognizer(guest)
         }
-        titleScroll.contentSize = CGSizeMake(100 * 7, 0)
-        
-        contentScroll.contentSize = CGSizeMake(SCREENW * 7, 0)
+        titleScroll.contentSize = CGSize(width: UIScreen.main.bounds.width, height: 0)
+
+        contentScroll.contentSize = CGSize(width: UIScreen.main.bounds.width * 7, height: 0)
         contentScroll.delegate = self
     }
     
-    func labelTap(tap: UITapGestureRecognizer)
-    {
+    @objc func labelTap(tap: UITapGestureRecognizer) {
         let index = CGFloat((tap.view?.tag)! - 100)
         var offset: CGPoint = contentScroll.contentOffset
-        let offsetX = SCREENW * index
+        let offsetX = UIScreen.main.bounds.width * index
         offset.x = offsetX
         
         contentScroll.setContentOffset(offset, animated: true)
     }
 }
 
- //MARK: UIScrollViewDelegate
+ // MARK: - UIScrollViewDelegate
 
-extension TMHomeController: UIScrollViewDelegate
-{
-    func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView)
-    {
+extension TMHomeController: UIScrollViewDelegate {
+
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+
         let offsetX = scrollView.contentOffset.x
         let height = scrollView.frame.size.height
         let width = scrollView.frame.size.width
-        
-        
-        let index = Int(offsetX / SCREENW)
+
+        let index = Int(offsetX / UIScreen.main.bounds.width)
         
         //设置标题滚动条随内容滚动而滚动
         var titleOffset = titleScroll.contentOffset
         let titleLabel = titleScroll.subviews[index] as! UILabel
-        titleLabel.textColor = UIColor.redColor()
+        titleLabel.textColor = UIColor.red
         var titleOffsetX = titleLabel.frame.origin.x + titleLabel.frame.size.width * 0.5 - scrollView.frame.size.width * 0.5
         let maxTitleOffset = titleScroll.contentSize.width - width
-        if titleOffsetX < 0
-        {
+
+        if titleOffsetX < 0 {
             titleOffsetX = 0
         }
-        if titleOffsetX > maxTitleOffset
-        {
+
+        if titleOffsetX > maxTitleOffset {
             titleOffsetX = maxTitleOffset
         }
+
         titleOffset.x = titleOffsetX
         titleScroll.setContentOffset(titleOffset, animated: true)
         
         //修改中间title显示颜色不正确的bug
-        for subLabel in titleScroll.subviews
-        {
-            if subLabel.isKindOfClass(UILabel){
+        for subLabel in titleScroll.subviews {
+            if subLabel.isKind(of: UILabel.self) {
                 let label = subLabel as! UILabel
                 if label != titleLabel{
-                    label.textColor = UIColor.blackColor()
+                    label.textColor = UIColor.black
                 }
             }
         }
         let willShowVC = childViewControllers[index] as! MyTableViewController
         //如果控制器已经加载了,就不要再次计算frame了
-        if willShowVC.isViewLoaded(){return}
+        if willShowVC.isViewLoaded { return }
 
-        willShowVC.view.frame = CGRectMake(offsetX, 0, width, height)
+        willShowVC.view.frame = CGRect(x: offsetX, y: 100, width: width, height: height)
         
         scrollView.addSubview(willShowVC.view)
-        
-        
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView)
-    {
-        let scale = scrollView.contentOffset.x/scrollView.frame.size.width
-        
-        let leftIndex = Int(scale)
-        var rightIndex : Int = 0
-        
-        //滑动当前对应的前一个label和后一个label
-        if leftIndex < 6 {
-            rightIndex = leftIndex + 1
-        }
-        let leftLabel = titleScroll.subviews[leftIndex] as! UILabel
-        let rightLabel = titleScroll.subviews[rightIndex] as! UILabel
-        
-        let rightScale = scale - CGFloat(leftIndex)
-        let leftScale = 1 - rightScale
-        
-        //到达边界继续滚动的话直接返回,不做处理
-        if scale < 1 || scale > 6 {return}
-        
-        leftLabel.textColor = UIColor.init(red: leftScale, green: 0, blue: 0, alpha: 1.0)
-        rightLabel.textColor = UIColor.init(red: rightScale, green: 0, blue: 0, alpha: 1.0)
-        
-        debugPrint("左:\(leftScale),右:\(rightScale)")
         
     }
     
     //用户手动滑动scrollView停止时候执行的方法
     
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView)
-    {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         scrollViewDidEndScrollingAnimation(scrollView)
     }
 }
-
-
- //MARK: extension Selector
-
-private extension Selector
-{
-    static let labelTap =  #selector(TMHomeController.labelTap(_:))
-}
-
 
